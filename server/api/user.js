7a7e4617728c11b1eda28b8431cc383efbbc7553
Pooja Srivastava <bioinfor.pooja@gmail.com>
2018-01-09 18:55:11 -0500

    added session related middleware

--- server/api/user.js
+++ server/api/user.js
@@ -30,7 +30,9 @@ router.get('/:id', (req, res, next) => {
     .then(requestedUser => res.json(requestedUser))
     .catch(next);
 });
-
+/*debounce-
+Useful for implementing behavior that should only happen after a repeated action has completed i.e 
+debounce is a technique to make sure a function cannot be invoked repeatedly within a short period. */
 router.put('/:id', (req, res, next) => {
   req.requestedUser.update(req.body)
     .then(user => res.json(user))
--- server/index.js
+++ server/index.js
@@ -3,11 +3,19 @@ const app = express();
 const path = require('path');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 /* "Enhancing" middleware (does not send response, server-side effects only) */
 app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	//this mandatory configuration ensures that session IDs are not predictable
+	secret: 'tongiscool',
+	//this option is recommended and reduces session concurrency issues
+	resave: false
+}));
+
 
 /* "Responding" middleware (may send a response back to client) */
 app.use('/api', require('./api'));
