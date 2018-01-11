c9bff6b256574e2f6ca66aa7f4eccaa052f2d574
Pooja Srivastava <bioinfor.pooja@gmail.com>
2018-01-11 16:56:05 -0500

    attaching user to a session

--- server/index.js
+++ server/index.js
@@ -4,21 +4,51 @@ const path = require('path');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const User = require('./db/models/user');
+
 
 /* "Enhancing" middleware (does not send response, server-side effects only) */
 app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(session({
 	//this mandatory configuration ensures that session IDs are not predictable
 	secret: 'tongiscool',
 	//this option is recommended and reduces session concurrency issues
-	resave: false
+	resave: false,
+	saveUninitialized: true
 }));
+
+app.post('/login', function (req, res, next) {
+	User.findOne({
+		where: req.body
+	})
+	.then(function(user){
+		if(!user){
+			res.sendStatus(401);
+		}else{
+			req.session.userId = user.id;
+			res.sendStatus(200);
+		}
+	})
+	.catch(next);
+});
+// app.use(function(req, res, next) {
+// 	console.log('session', req.session);
+// 	next();
+// });
+
+// app.use('/api', function (req, res, next) {
+//   if (!req.session.counter) req.session.counter = 0;
+//   console.log('counter', ++req.session.counter);
+//   next();
+// });
 
 
 /* "Responding" middleware (may send a response back to client) */
 app.use('/api', require('./api'));
+
 
 const validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 const indexPath = path.join(__dirname, '../public/index.html');
